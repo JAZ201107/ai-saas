d2acba22008d85e7e80f5d8b6a40f1becf66fde5
zhang yuyang <zhangyuyang1211@gmail.com>
2023-07-22 05:49:55 +0800

    Add Sidebar Active Function

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,6 +13,7 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
@@ -20,8 +21,6 @@ const montserrat = Montserrat({
   weight: "600",
   subsets: ["latin"],
 });
-
-interface SidebarProps {}
 
 const routes = [
   {
@@ -67,7 +66,8 @@ const routes = [
   },
 ];
 
-const Sidebar: FC<SidebarProps> = ({}) => {
+const Sidebar: FC = ({}) => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white ">
       <div className="px-3 py-2 flex-1">
@@ -85,7 +85,12 @@ const Sidebar: FC<SidebarProps> = ({}) => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h05 w05 mr-3", route.color)} />
