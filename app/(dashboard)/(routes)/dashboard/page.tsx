8eb82ba64146c02a810178f0f827a495723e8852
zhang yuyang <zhangyuyang1211@gmail.com>
2023-07-22 05:45:09 +0800

    Sidebar Finished

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,9 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/clerk-react";
-import Image from "next/image";
 
 export default function DashboardPage() {
   return (
     <div>
       <p> Danshboard Page</p>
-      <UserButton></UserButton>
     </div>
   );
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,20 @@
-"use client";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 import { ReactNode } from "react";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
-  <div className="h-full relative">
-    <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900 md:w-72">
-      <div>Hello Sidebar</div>
-    </div>
+  return (
+    <div className="h-full relative">
+      <div className="hidden h-full md:flex md:flex-col md:w-72 md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        <Sidebar />
+      </div>
 
-    <main className="md:pl-72">
-      <Navbar />
-      Hello Content
-      {children}
-    </main>
-  </div>;
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default DashboardLayout;
