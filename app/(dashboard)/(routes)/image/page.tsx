a3cdfbd5ba38e820d1c84f3adf15cdbd97127274
zhang yuyang <zhangyuyang1211@gmail.com>
2023-08-02 03:47:37 +0800

    Image Generation AI Finished

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Code, ImageIcon, MessageSquare } from "lucide-react";
+import { Code, Download, ImageIcon, MessageSquare } from "lucide-react";
 import { useForm } from "react-hook-form";
 
 import { amountOptions, formSchema, resolutionOptions } from "./constants";
@@ -26,6 +26,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 
 const ImagePage = () => {
   const router = useRouter();
@@ -168,7 +170,26 @@ const ImagePage = () => {
           {images.length === 0 && !isLoading && (
             <Empty label="No images generated" />
           )}
-          <div>Image will be render here</div>
+          <div className="grid grid-cols-1 mg:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+            {images.map((src) => (
+              <Card key={src} className="rounded-lg overflow-hidden">
+                <div className="relative aspect-square">
+                  <Image alt="Image" fill src={src} />
+                </div>
+
+                <CardFooter className="p-2">
+                  <Button
+                    variant="secondary"
+                    className="w-full"
+                    onClick={() => window.open(src)}
+                  >
+                    <Download className="h-4 w-4 mr-2" />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
